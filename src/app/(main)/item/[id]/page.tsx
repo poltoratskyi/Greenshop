8872ade325c9130573b10ebb5c724d708aed918f
Alexander Poltoratskyi <poltoratskyialexander@gmail.com>
2024-11-28 22:35:19 +0200

    fix prop

--- src/app/(main)/item/[id]/page.tsx
+++ src/app/(main)/item/[id]/page.tsx
@@ -3,11 +3,11 @@ import { notFound } from "next/navigation";
 import SingleItem from "../../../../components/shared/single-item/single-item";
 
 interface Props {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default async function Item({ params }: Props) {
-  const { id } = params;
+  const { id } = await params;
 
   const item = await prisma.item.findFirst({
     where: { id: Number(id) },
