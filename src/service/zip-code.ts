02aee61df4bc59db85a6490dbdbe85326e9870cd
Alexander Poltoratskyi <poltoratskyialexander@gmail.com>
2025-02-24 12:13:40 +0200

    fix zipcode req

--- src/service/zip-code.ts
+++ src/service/zip-code.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const fetchZipCode = async (zipCode: string): Promise<ZipCodeData> => {
   const response = await axios.get<ZipCodeData>(
-    `http://api.zippopotam.us/us/${zipCode}`
+    `https://api.zippopotam.us/us/${zipCode}`
   );
   return response.data;
 };
